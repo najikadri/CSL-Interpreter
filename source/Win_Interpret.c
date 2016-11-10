c9b9944911cada04ea021c67e72af7bcdc84974a
Naji Kadri <naji-newman-2000@hotmail.com>
2016-11-10 19:52:23 +0200

    csli path bug fix
    Fixed a bug that prevented clsi.exe to run from different directories. Basically the code was running the python file from the same directory of execution which is wrong we need to execute it from the path of the clsi.exe location where the python file (python interpreter) is found.

--- source/Win_Interpret.c
+++ source/Win_Interpret.c
@@ -11,17 +11,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 void run (char *command);
+void getDir (char* path,char* dir);
 
 int main (int argc, char *argv[]){
-	char command[100];
+	char dir[1024]; //the directory path
+	char command[100]; //the command to be executed
+	char* path; //the typical path variable
+	path = getenv("PATH"); //get the environment PATH variable
+	getDir(path,dir); //search for the right directory from PATH
+
 	if (argc == 1){
-		sprintf(command,"py Calcinter.py");
+		sprintf(command,"py \"%s\\Calcinter.py\"",dir);
 		run(command);
 	}else{
-		sprintf(command,"py Calcinter.py \"%s\"",argv[1]);
+		sprintf(command,"py \"%s\\Calcinter.py\" \"%s\"",dir,argv[1]);
 		run(command);
 		getch();
 	}
@@ -31,4 +37,18 @@ int main (int argc, char *argv[]){
 void run (char *command){
 	system("TITLE Calculator Simple Language Interpreter");
 	system(command);
+}
+
+void getDir (char* path,char* dir) {
+	char *token;
+	token = strtok(path,";");
+
+	while (token != NULL){
+		if(strstr(token, "CSL") != NULL || strstr(token, "csl") != NULL ) {
+			strcpy(dir,token);
+			break;
+		}else{
+			token = strtok(NULL,";");
+		}
+	}
 }
